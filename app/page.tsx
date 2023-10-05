528f7f50d08bcc4ae2e07204cefb0425202c96f6
langeblake <114412977+langeblake@users.noreply.github.com>
2023-10-05 21:58:58 +0900

    Page.tsx revert syntax

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 
-
-import Image from 'next/image'
-import styles from './page.module.css'
-import ClientOnly from './components/ClientOnly'
-import Container from './components/Container'
-import EmptyState from './components/EmptyState'
-import getListings, { IListingsParams } from './actions/getListings'
-import ListingCard from './components/listings/ListingCard'
-import getCurrentUser from './actions/getCurrentUser'
+import Image from 'next/image';
+import styles from './page.module.css';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import getListings, { IListingsParams } from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
 
 interface HomeProps {
--- app/types/index.ts
+++ app/types/index.ts
@@ -29,3 +29,4 @@ export type SafeUser = Omit<
 // Omit<T, K>: This utility type creates a new type by taking an existing type T and omitting properties specified by K.
 // The & then adds back the createdAt property, but this time as a string type.
 // The reason for this is that the createdAt in the original Listing type is a Date object, but for the purposes of safeListing, it's represented as a string.
+
