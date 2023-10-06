c4c563b4f8dacad5874637f99be01e1c4d16a412
langeblake <114412977+langeblake@users.noreply.github.com>
2023-10-06 09:06:34 +0900

    Added logo-icon for small screens

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,14 +7,24 @@ const Logo = () => {
     const router = useRouter();
 
     return (
-        <Image
-            onClick={() => router.push('/')}
-            alt="Logo"
-            className="hidden md:block cursor-pointer"
-            height="200"
-            width="200"
-            src="/images/logo.png"
-            />
+        <>
+            <Image
+                onClick={() => router.push('/')}
+                alt="Logo"
+                className="hidden md:block cursor-pointer"
+                height="250"
+                width="250"
+                src="/images/logo.png"
+                />
+            <Image
+                onClick={() => router.push('/')}
+                alt="Logo-icon"
+                className="block md:hidden cursor-pointer"
+                height="40"
+                width="40"
+                src="/images/logo-icon.png"
+                />
+        </>
     )
 }
 
